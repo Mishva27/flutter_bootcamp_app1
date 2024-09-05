593413909869a68bae2ea359924342fdebb9d5d4
Mishva Sankhavara <mishvasankhavara@gmail.com>
2024-09-05 18:28:31 +0530

    Flutter bootcamp 1st app completed.

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
@@ -40,21 +41,23 @@ class MyApp extends StatelessWidget {
                   color: Colors.teal.shade100,
                 ),
               ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
               Card(
-                color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                 child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
+                padding: const EdgeInsets.all(0.0),
+                child: ListTile(
+                  leading: Icon(
                       Icons.phone,
                       color: Colors.teal,
                     ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
+                    title: Text(
                       '+91 123489675',
                       style: TextStyle(
                         color: Colors.teal.shade900,
@@ -62,15 +65,27 @@ class MyApp extends StatelessWidget {
                         fontSize: 20.0,
                       ),
                     ),
-                  ],
-                ),),
+                ),
+                ),
               ),
               Card(
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                color: Colors.white,
                 child: Padding(
-                  padding: EdgeInsets.all(10.0),
-                child: 
+                  padding: EdgeInsets.all(0.0),
+                child: ListTile(
+                  leading: Icon(
+                      Icons.email,color: 
+                      Colors.teal,
+                    ),
+                  title: Text(
+                      'example@gmail.ocm',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 20.0,
+                        fontFamily: 'Source Sans 3',
+                      ),
+                    ),
+                ),
                 ),
               ),
             ],
@@ -81,22 +96,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
-Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.email,color: 
-                      Colors.teal,
-                    ),
-                    SizedBox(width: 10.0,),
-                    Text(
-                      'example@gmail.ocm',
-                      style: TextStyle(
-                        color: Colors.teal.shade900,
-                        fontSize: 20.0,
-                        fontFamily: 'Source Sans 3',
-                      ),
-                    ),
-                  ],
-                ),
